c8fba7ae8ff28574d44f5218412178318bbecb32
julio <jnandrade@uc.cl>
2022-11-05 22:58:28 -0300

    feat/Get all restaurants

--- backend/restaurants/views.py
+++ backend/restaurants/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Restaurants
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the restaurants index.")
+    latest_restaurants_list = Restaurants.objects.order_by('-created_at')
+    output = ', '.join([r.name for r in latest_restaurants_list])
+    return HttpResponse(output)
