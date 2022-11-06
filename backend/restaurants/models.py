0331441dda767863c98bc0f15a5126f3707c9d5d
julio <jnandrade@uc.cl>
2022-11-05 22:24:49 -0300

    Django initial setup & restaurant model

---
+++ backend/restaurants/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Restaurants(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=70)
+    food_type = models.CharField(max_length=50)
+    rating = models.FloatField(null=True)
+    rating_votes = models.IntegerField(default=0)
+    checkbox = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
