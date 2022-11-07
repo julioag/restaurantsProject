cdf9868681a26820644a3a32ba6dccc5c66c5f83
julio <jnandrade@uc.cl>
2022-11-07 18:00:31 -0300

    backend: Feat/endpoints deliver object id

--- backend/restaurant_api/serializers.py
+++ backend/restaurant_api/serializers.py
@@ -4,4 +4,4 @@ from .models import Restaurant
 class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
-        fields = ["name", "location", "food_type", "rating", "rating_votes", "checkbox", "created_at", "updated_at"]
+        fields = ["id", "name", "location", "food_type", "rating", "rating_votes", "checkbox", "created_at", "updated_at"]
