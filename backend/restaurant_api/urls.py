1568233d53ad7aa326f25087da294650783547fc
julio <jnandrade@uc.cl>
2022-11-07 16:06:57 -0300

    backend: Feat/ GET & POST endpoints

--- backend/restaurant_api/urls.py
+++ backend/restaurant_api/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = []
+from django.urls import path, include
+from .views import (RestaurantList)
+
+urlpatterns = [
+  path('', RestaurantList.as_view()),
+]
--- backend/restaurant_api/views.py
+++ backend/restaurant_api/views.py
@@ -6,3 +6,22 @@ from .models import Restaurant
 from .serializers import RestaurantSerializer
 
 # Create your views here.
+class RestaurantList(APIView):
+    # permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        restaurants = Restaurant.objects.all()
+        serializer = RestaurantSerializer(restaurants, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        data = {
+            "name": request.data.get("name"),
+            "location": request.data.get("location"),
+            "food_type": request.data.get("food_type"),
+        }
+        serializer = RestaurantSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
